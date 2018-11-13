b07c84b3477239dc819084394df5a40a8046ca5b
Ty Richadson <tyLrichardson63@gmail.com>
2018-11-13 16:53:57 -0600

    completed angular-chartjs

--- server/public/scripts/controllers/reports.controller.js
+++ server/public/scripts/controllers/reports.controller.js
@@ -8,50 +8,18 @@ timeTrackerApp.controller('ReportsController', ['TimeTrackerAppService',
 
   //Link view to service data
 
-  self.addProject = timeTrackerAppService.addProject;
+  self.resProject = timeTrackerAppService.resProject;
 
+  self.reportData = timeTrackerAppService.reportData;
 
-  let projectNames = ["w", "x", "y"];
-  let projectTimeSums = [1.5, 4.5, 6];
+  self.reportNames = timeTrackerAppService.reportNames;
 
+  self.reportSum = timeTrackerAppService.reportSum;
 
-  let ctx = angular.element(document.querySelector('#myChart'));
-  let myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: projectNames,
-      datasets: [{
-        label: 'Total Hours Burned per Project',
-        data: projectTimeSums,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
-  });
+  self.ctx = timeTrackerAppService.ctx;
 
+  self.myChart = timeTrackerAppService.myChart;
+
+  
 }]);
 
--- server/public/scripts/services/timeTracker.service.js
+++ server/public/scripts/services/timeTracker.service.js
@@ -3,9 +3,21 @@ timeTrackerApp.service('TimeTrackerAppService', ['$http', function ($http) {
 
   let self = this;
 
-  self.addEntry = { list: [] };
+  self.resEntry = { list: [] };
 
-  self.addProject = { list: [] };
+  self.resProject = { list: [] };
+
+  self.reportData;
+
+  self.reportNames;
+
+  self.reportHours;
+
+  self.reportSum;
+
+  self.ctx;
+
+  self.myChart;
 
   //GET getEntry populates entry history
   self.getEntry = function () {
@@ -14,7 +26,7 @@ timeTrackerApp.service('TimeTrackerAppService', ['$http', function ($http) {
       url:'/addEntry'
     }).then((response) => {
       console.log('getEntry response from service', response);
-      self.addEntry.list = response.data;
+      self.resEntry.list = response.data;
     })
     .catch((error) => {
       console.log('getEntry error in service', error);
@@ -46,8 +58,54 @@ timeTrackerApp.service('TimeTrackerAppService', ['$http', function ($http) {
       url:'/manageProjects'
       }).then((response) => {
       console.log('getProject response timeTrackerService', response);
-      self.addProject.list = response.data;
-      console.log('getProject in timeTrackerService vm.addProject.list =  response.data', response.data);
+      self.resProject.list = response.data;
+      self.reportData = response.data;
+      console.log('getProject in timeTrackerService vm.resProject.list =  response.data', response.data);
+      console.log('reportData', self.reportData);
+      reportNames = self.reportData.map(names => names.project_name);
+      console.log('reportNames array', reportNames);
+      reportHours = self.reportData.map(hours => (hours.sum));
+      console.log('reportHours array', reportHours);
+      reportSum = reportHours.map(sum => parseInt(sum, 10));
+      console.log('reportSum array', reportSum);
+      ctx = angular.element(document.querySelector('#myChart'));
+      myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: reportNames,
+          datasets: [{
+            label: 'Total Hours Burned per Project',
+            data: reportSum,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      });
+
     })
     .catch((error) => {
       console.log('getProject error in timeTrackerService', error);
